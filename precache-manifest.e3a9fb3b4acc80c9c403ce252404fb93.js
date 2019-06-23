1bae72cae6ff013e0044d1debb83699bb4d32dca
Damian Lupa <damian.lupa88@gmail.com>
2019-06-23 21:58:41 +0200

    Updates

---
+++ precache-manifest.e3a9fb3b4acc80c9c403ce252404fb93.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d21ed145d1915766c7cf5301464b947",
+    "url": "/air-quality/index.html"
+  },
+  {
+    "revision": "2f7c7df2ae4ee3adaeb6",
+    "url": "/air-quality/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "482cce7d7d827d71ce43",
+    "url": "/air-quality/static/css/main.d9553985.chunk.css"
+  },
+  {
+    "revision": "2f7c7df2ae4ee3adaeb6",
+    "url": "/air-quality/static/js/2.e87c2992.chunk.js"
+  },
+  {
+    "revision": "482cce7d7d827d71ce43",
+    "url": "/air-quality/static/js/main.018a8806.chunk.js"
+  },
+  {
+    "revision": "3f9210d8381d6773f5aa",
+    "url": "/air-quality/static/js/runtime~main.5984ee94.js"
+  },
+  {
+    "revision": "5199c3b016f3a8ca0931a26f6558cdc6",
+    "url": "/air-quality/static/media/background.5199c3b0.png"
+  },
+  {
+    "revision": "05ed983928e5bd570628b9301e3e05a5",
+    "url": "/air-quality/static/media/economic.05ed9839.png"
+  },
+  {
+    "revision": "882b8f4c0bdb47d847385ad4e2a836bb",
+    "url": "/air-quality/static/media/environmental.882b8f4c.png"
+  },
+  {
+    "revision": "dcdd071c427cdbacc9715f05dec819f4",
+    "url": "/air-quality/static/media/health.dcdd071c.png"
+  },
+  {
+    "revision": "f9d3ec8a30d6ce997c34502896a69e39",
+    "url": "/air-quality/static/media/logo.f9d3ec8a.png"
+  }
+]);
