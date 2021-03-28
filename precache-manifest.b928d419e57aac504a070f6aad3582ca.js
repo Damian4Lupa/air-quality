f326869fdb85969d0bce1153e036fb6b99edebf4
Damian Lupa <damian.lupa88@gmail.com>
2021-03-28 16:26:01 +0200

    Updates

---
+++ precache-manifest.b928d419e57aac504a070f6aad3582ca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b04d340bc97e3fd8afe775f0b113f5e",
+    "url": "/air-quality/index.html"
+  },
+  {
+    "revision": "3cde19af129633aac4b0",
+    "url": "/air-quality/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "863ce1a65908500f5aeb",
+    "url": "/air-quality/static/css/main.6549f003.chunk.css"
+  },
+  {
+    "revision": "3cde19af129633aac4b0",
+    "url": "/air-quality/static/js/2.2dabdcb0.chunk.js"
+  },
+  {
+    "revision": "863ce1a65908500f5aeb",
+    "url": "/air-quality/static/js/main.5559fd3c.chunk.js"
+  },
+  {
+    "revision": "3f9210d8381d6773f5aa",
+    "url": "/air-quality/static/js/runtime~main.5984ee94.js"
+  },
+  {
+    "revision": "ebd82c2a0ec60c500350f62dfdd4dbd9",
+    "url": "/air-quality/static/media/background-1000.ebd82c2a.jpg"
+  },
+  {
+    "revision": "7fe97162acc21ec1557c578e7eaa6b76",
+    "url": "/air-quality/static/media/background-1500.7fe97162.jpg"
+  },
+  {
+    "revision": "71f864d0e68e2521dd45d88c3a7a0888",
+    "url": "/air-quality/static/media/background-500.71f864d0.jpg"
+  },
+  {
+    "revision": "05ed983928e5bd570628b9301e3e05a5",
+    "url": "/air-quality/static/media/economic.05ed9839.png"
+  },
+  {
+    "revision": "882b8f4c0bdb47d847385ad4e2a836bb",
+    "url": "/air-quality/static/media/environmental.882b8f4c.png"
+  },
+  {
+    "revision": "dcdd071c427cdbacc9715f05dec819f4",
+    "url": "/air-quality/static/media/health.dcdd071c.png"
+  },
+  {
+    "revision": "607344da2d33ed006c667166313da92c",
+    "url": "/air-quality/static/media/info.607344da.svg"
+  },
+  {
+    "revision": "f9d3ec8a30d6ce997c34502896a69e39",
+    "url": "/air-quality/static/media/logo.f9d3ec8a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/air-quality/precache-manifest.76f1164976a7283d02369add9e914779.js"
+  "/air-quality/precache-manifest.b928d419e57aac504a070f6aad3582ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/air-quality/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
