2cd8d8aa9e4bdaf9792b096a1922206f07eca657
Damian Lupa <damian.lupa88@gmail.com>
2021-03-28 16:49:11 +0200

    css code refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./layouts/index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
